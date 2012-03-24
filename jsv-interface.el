11abbbf5a15f638cc99d1bf7756f9a7be3e9130c
James Vasak <jvasak@vrinc.com>
2012-03-24 16:29:28 -0400

    Update frame alpha and color-themes

--- jsv-interface.el
+++ jsv-interface.el
@@ -12,29 +12,31 @@
 (setq transient-mode-mark t)
 
 ;; lower alpha when frame is deselected
-(set-frame-parameter (selected-frame) 'alpha '(85 50))
+;; (set-frame-parameter (selected-frame) 'alpha '(85 50))
 
 ;; some reasonable frame size/font defaults
 (setq default-frame-alist
       '((width . 120)
 	(height . 74) 
-	(alpha 85 50) 
+	;;(alpha 85 50) 
 	(font . "Liberation Mono-10:regular")))
 
 ;; set up pleasing color theme
 (require 'color-theme)
 (color-theme-initialize)
 
-(color-theme-jsc-dark)
+(color-theme-charcoal-black)
 
 (global-set-key (kbd "C-<kp-1>") 'color-theme-calm-forest)
-(global-set-key (kbd "C-<kp-2>") 'color-theme-blackboard)
-(global-set-key (kbd "C-<kp-3>") 'color-theme-jsc-dark)
-(global-set-key (kbd "C-<kp-4>") 'color-theme-subtle-hacker)
-(global-set-key (kbd "C-<kp-5>") 'color-theme-robin-hood)
-(global-set-key (kbd "C-<kp-6>") 'color-theme-greiner)
-(global-set-key (kbd "C-<kp-7>") 'color-theme-taylor)
-(global-set-key (kbd "C-<kp-8>") 'color-theme-taming-mr-arneson)
+(global-set-key (kbd "C-<kp-2>") 'color-theme-charcoal-black)
+(global-set-key (kbd "C-<kp-3>") 'color-theme-blackboard)
+(global-set-key (kbd "C-<kp-4>") 'color-theme-gtk-ide)
+(global-set-key (kbd "C-<kp-5>") 'color-theme-emacs-21)
+(global-set-key (kbd "C-<kp-6>") 'color-theme-gnome2)
+(global-set-key (kbd "C-<kp-7>") 'color-theme-subtle-hacker)
+(global-set-key (kbd "C-<kp-8>") 'color-theme-jsc-dark)
+(global-set-key (kbd "C-<kp-9>") 'color-theme-kingsajz)
+
 
 ;;(if window-system
 ;;    (color-theme-greiner)
