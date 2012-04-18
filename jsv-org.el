b07d0886d289a555b8724b60346367703b9f4813
James Vasak <jvasak@vrinc.com>
2012-04-18 08:17:28 -0400

    Update org-mode config for more export options

--- jsv-org.el
+++ jsv-org.el
@@ -6,6 +6,14 @@
 
 (require 'org-install)
 
+(add-to-list 'load-path "~/emacs/org-mode/contrib/lisp/")
+(require 'org-export-generic)
+(require 'org-checklist)
+(require 'htmlize)
+
+;; ditaa
+(setq org-ditaa-jar-path "~/emacs/org-mode/contrib/scripts/ditaa.jar")
+
 (setq org-drawers '("PROPERTIES" "CLOCK" "LOGBOOK"))
 (setq org-log-into-drawer t)
 ;; (setq org-clock-into-drawer t)
@@ -67,7 +75,7 @@
 
 ;; Custom key bindings
 (global-set-key (kbd "<f12>")   'org-agenda)
-(global-set-key (kbd "<S-f12>") 'org-capture)
+(global-set-key (kbd "<f11>")   'org-capture)
 (global-set-key (kbd "<f5>")    'org-clock-in)
 (global-set-key (kbd "<f6>")    'org-clock-goto)
 (global-set-key (kbd "<f7>")    'org-clock-out)
@@ -204,17 +212,6 @@
   (not (string= lang "dot"))) ; don't ask for dot
 (setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
 
-
-;; ditaa
-(setq org-ditaa-jar-path "~/bin/ditaa0_6b.jar")
-
-
-;;
-;; User contrib to reset checklists on recrring tasks
-;;
-(load "~/Dropbox/gtd/org-checklist.el")
-(load "~/Dropbox/gtd/htmlize.el")
-
 ;; Publishing set up:
 ;;     website - publish to cleopatra
 ;;     web-dev - publish to local path (testing)
@@ -265,3 +262,92 @@
          :publishing-directory "~/public_html/other/"
          :publishing-function org-publish-attachment)
         ("web-dev" :components ("org-dev" "img-dev" "oth-dev"))))
+
+(require 'org-latex)
+(setq org-export-latex-listings t)
+
+
+;; Originally taken from Bruno Tavernier: http://thread.gmane.org/gmane.emacs.orgmode/31150/focus=31432
+;; but adapted to use latexmk 4.20 or higher.
+(defun my-auto-tex-cmd ()
+  "When exporting from .org with latex, automatically run latex,
+     pdflatex, or xelatex as appropriate, using latexmk."
+  (let ((texcmd)))
+  ;; default command: oldstyle latex via dvi
+  (setq texcmd "latexmk -dvi -pdfps -quiet %f")
+  ;; pdflatex -> .pdf
+  (if (string-match "LATEX_CMD: pdflatex" (buffer-string))
+      (setq texcmd "latexmk -pdf -quiet %f"))
+  ;; xelatex -> .pdf
+  (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+      (setq texcmd "latexmk -pdflatex=xelatex -pdf -quiet %f"))
+  ;; LaTeX compilation command
+  (setq org-latex-to-pdf-process (list texcmd)))
+
+(add-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-cmd)
+
+
+;; Specify default packages to be included in every tex file, whether pdflatex or xelatex
+(setq org-export-latex-packages-alist
+      '(("" "graphicx" t)
+            ("" "longtable" nil)
+            ("" "float" nil)))
+
+(defun my-auto-tex-parameters ()
+      "Automatically select the tex packages to include."
+      ;; default packages for ordinary latex or pdflatex export
+      (setq org-export-latex-default-packages-alist
+            '(("AUTO" "inputenc" t)
+              ("T1"   "fontenc"   t)
+              (""     "fixltx2e"  nil)
+              (""     "wrapfig"   nil)
+              (""     "soul"      t)
+              (""     "textcomp"  t)
+              (""     "marvosym"  t)
+              (""     "wasysym"   t)
+              (""     "latexsym"  t)
+              (""     "amssymb"   t)
+              (""     "hyperref"  nil)))
+
+      ;; Packages to include when xelatex is used
+      (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+          (setq org-export-latex-default-packages-alist
+                '(("" "fontspec" t)
+                  ("" "xunicode" t)
+                  ("" "url" t)
+                  ("" "rotating" t)
+                  ("american" "babel" t)
+                  ("babel" "csquotes" t)
+                  ("" "listings" nil)
+                  ("" "soul" t)
+                  ("xetex" "hyperref" nil)
+                  )))
+
+
+
+      (if (string-match "LATEX_CMD: xelatex" (buffer-string))
+          (setq org-export-latex-classes
+                (cons '("article"
+                        "\\documentclass[11pt,oneside]{article}"
+                        ("\\section{%s}" . "\\section*{%s}")
+                        ("\\subsection{%s}" . "\\subsection*{%s}")
+                        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                        ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                        ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+                      org-export-latex-classes))))
+
+(add-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-parameters)
+
+
+(add-to-list 'org-export-latex-classes
+          '("org-article"
+             "\\documentclass{org-article}
+             [NO-DEFAULT-PACKAGES]
+             [PACKAGES]
+             [EXTRA]"
+             ("\\section{%s}" . "\\section*{%s}")
+             ("\\subsection{%s}" . "\\subsection*{%s}")
+             ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+             ("\\paragraph{%s}" . "\\paragraph*{%s}")
+             ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
