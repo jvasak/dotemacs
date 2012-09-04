47fc8abde4520811952dfda44848cd371b65db02
James Vasak <jvasak@vrinc.com>
2012-09-04 09:18:58 -0400

    Add support for asciidoc

---
+++ jsv-asciidoc.el
@@ -0,0 +1,6 @@
+(autoload 'doc-mode "doc-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.adoc$" . doc-mode))
+(add-hook 'doc-mode-hook
+	  '(lambda ()
+	     (turn-on-auto-fill)
+	     (require 'asciidoc)))
