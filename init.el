a7e3cd32a7e7264aaf70855e6a896499e3f51f6e
James Vasak <jvasak@vrinc.com>
2014-09-26 08:12:25 -0400

    Disable iswitchb in favor of helm-mini

--- init.el
+++ init.el
@@ -240,7 +240,8 @@
 
    (:name helm
 	  :after (progn
-		   (helm-mode t)))
+		   (helm-mode t)
+		   (global-set-key "\C-xb" 'helm-mini)))
 
    helm-ls-git
 
@@ -324,16 +325,17 @@
 ;; Buffer management
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; iswitchb niceness
-(iswitchb-mode 1)
-(defun iswitchb-local-keys ()
-  (mapc (lambda (K)
-	  (let* ((key (car K)) (fun (cdr K)))
-	    (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
-	'(("<right>" . iswitchb-next-match)
-	  ("<left>"  . iswitchb-prev-match)
-	  ("<up>"    . ignore             )
-	  ("<down>"  . ignore             ))))
-(add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
+;; (iswitchb-mode 1)
+;; (setq iswitchb-default-method 'maybe-frame)
+;; (defun iswitchb-local-keys ()
+;;   (mapc (lambda (K)
+;; 	  (let* ((key (car K)) (fun (cdr K)))
+;; 	    (define-key iswitchb-mode-map (edmacro-parse-keys key) fun)))
+;; 	'(("<right>" . iswitchb-next-match)
+;; 	  ("<left>"  . iswitchb-prev-match)
+;; 	  ("<up>"    . ignore             )
+;; 	  ("<down>"  . ignore             ))))
+;; (add-hook 'iswitchb-define-mode-map-hook 'iswitchb-local-keys)
 
 ;; Clean up the current emacs session, when needed
 ;; look at (require 'midnight) at some point, too
